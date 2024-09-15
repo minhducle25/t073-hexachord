2057a061112e1e6a4912635318858d6a3002f757
minhducle25 <lducminh25@gmail.com>
2024-09-16 00:46:08 +1000

    dichord added

---
+++ src/components/Chords/Dichords.jsx
@@ -0,0 +1,62 @@
+// Dichords.jsx
+
+import React, { useRef } from 'react';
+import ToneGenerator from '../ToneGenerator'; // Import ToneGenerator
+
+const Dichords = ({ points, onSideClick }) => {
+  const toneGeneratorRef = useRef(null); // Create a ref for ToneGenerator
+
+  // Function to calculate the midpoint of a line segment
+  const calculateMidpoint = (x1, y1, x2, y2) => {
+    return {
+      x: (x1 + x2) / 2,
+      y: (y1 + y2) / 2,
+    };
+  };
+
+  // Convert points string to array of objects
+  const pointsArray = points.split(' ').map(point => {
+    const [x, y] = point.split(',').map(Number);
+    return { x, y };
+  });
+
+  // Generate paths and circles for each side of the polygon
+  const sides = pointsArray.map((point, index) => {
+    const nextPoint = pointsArray[(index + 1) % pointsArray.length];
+    const midpoint = calculateMidpoint(point.x, point.y, nextPoint.x, nextPoint.y);
+
+    return (
+      <g key={index} onClick={() => {
+        const notes = onSideClick(index);
+        if (toneGeneratorRef.current) {
+          toneGeneratorRef.current.playChord(notes);
+        }
+      }}>
+        <path
+          d={`M${point.x},${point.y} L${nextPoint.x},${nextPoint.y}`}
+          className="stroke-transparent hover:stroke-violet-900"
+          strokeWidth="5"
+          cursor="pointer"
+          style={{ zIndex: 10, pointerEvents: 'all' }}
+        />
+        <circle
+          cx={midpoint.x}
+          cy={midpoint.y}
+          r="5"
+          className="fill-black hover:fill-indigo-500"
+          cursor="pointer"
+          style={{ zIndex: 10, pointerEvents: 'all' }}
+        />
+      </g>
+    );
+  });
+
+  return (
+    <>
+      {sides}
+      <ToneGenerator ref={toneGeneratorRef} /> {/* Add ToneGenerator component */}
+    </>
+  );
+};
+
+export default Dichords;
