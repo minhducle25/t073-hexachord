298276222b56834bc7703e6f0668dddce692d5eb
Duc Minh Le <122418348+minhducle25@users.noreply.github.com>
2024-10-16 11:36:05 +1000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="w-full pt-5 bg-transparent text-center">
-            <h1 className="text-4xl font-bold">THE DMPV</h1>
+            <h1 className="text-4xl font-bold">THE DMVP</h1>
         </div>
     );
 };
